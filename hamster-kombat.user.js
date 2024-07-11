9b5d1bdc0e1d49e381ca8c388ac2475f8bbfa50c
S3C237 <s3c237@gmail.com>
2024-07-11 22:15:41 +0330

    Update hamster-kombat.user.js

--- hamster-kombat.user.js
+++ hamster-kombat.user.js
@@ -7,9 +7,9 @@
 // @match        *://*.hamsterkombat.io/*
 // @grant        none
 // @icon         http://89.106.206.41/homous/HAMSTERICON.png
-// @downloadURL  https://github.com/amir-homous/Hamster-Kombat/raw/main/hamster-kombat.user.js
-// @updateURL    https://github.com/amir-homous/Hamster-Kombat/raw/main/hamster-kombat.user.js
-// @homepage     https://github.com/amir-homous/Hamster-Kombat
+// @downloadURL  https://github.com/S3C237/Hamster-Kombat/raw/main/hamster-kombat.user.js
+// @updateURL    https://github.com/S3C237/Hamster-Kombat/raw/main/hamster-kombat.user.js
+// @homepage     https://github.com/S3C237/Hamster-Kombat
 // ==/UserScript==
 
 (function() {
